245d2d79f5f159543787814f49ef598565921703
haubschueh <marco.weber.abcxyz@gmail.com>
2017-05-19 20:16:45 +0200

    Added a commandline sender class.

---
+++ src/main/java/ch/hslu/mobsys/manet/CommandLineMSN.java
@@ -0,0 +1,31 @@
+package ch.hslu.mobsys.manet;
+
+/**
+ * @author marco
+ */
+public class CommandLineMSN {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    private class udpSender implements Runnable {
+
+        private DatagramChannel udpChannel;
+
+        public udpSender(InetSocketAddress destination) {
+            try {
+                udpChannel = DatagramChannel.open();
+                udpChannel.socket().bind(destination);
+            } catch (IOException ex) {
+                System.err.println("");
+            }
+        }
+
+        public void run() {
+            //asdf
+        }
+    }
+     */
+}
