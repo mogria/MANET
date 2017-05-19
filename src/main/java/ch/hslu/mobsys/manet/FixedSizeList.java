e6b621f1cd190026ce4caafef3c73c55f0951ba1
haubschueh <marco.weber.abcxyz@gmail.com>
2017-05-19 18:51:19 +0200

    Updated package paths in the new files.

--- src/main/java/ch/hslu/mobsys/manet/FixedSizeList.java
+++ src/main/java/ch/hslu/mobsys/manet/FixedSizeList.java
@@ -1,4 +1,4 @@
-package main;
+package ch.hslu.mobsys.manet;
 
 import com.sun.javafx.collections.ObservableListWrapper;
 
--- src/main/java/ch/hslu/mobsys/manet/MulticastMessage.java
+++ src/main/java/ch/hslu/mobsys/manet/MulticastMessage.java
@@ -1,4 +1,4 @@
-package main;
+package ch.hslu.mobsys.manet;
 
 import javafx.beans.property.*;
 
