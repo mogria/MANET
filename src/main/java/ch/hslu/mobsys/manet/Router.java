82e9968461646b83b839b3fa348b79364043d91a
Mogria <m0gr14@gmail.com>
2017-05-19 19:51:20 +0200

    implemented resending and better exception handling

--- src/main/java/ch/hslu/mobsys/manet/Router.java
+++ src/main/java/ch/hslu/mobsys/manet/Router.java
@@ -1,6 +1,7 @@
 package ch.hslu.mobsys.manet;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
@@ -39,27 +40,32 @@ public class Router {
 
     public void run() throws IOException {
         udpChannel = DatagramChannel.open();
-        udpChannel.socket().bind(new InetSocketAddress(1337)); // 239.255.255.250
+        final InetAddress multicastAddr = InetAddress.getByAddress(new byte[]{(byte)239, (byte)255, (byte)255, (byte)250});
+        udpChannel.socket().bind(new InetSocketAddress(multicastAddr, 1337));
         // cisco7039-0360
         selector = Selector.open();
-        udpChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        udpChannel.register(selector, SelectionKey.OP_READ);
 
         while (true) {
             final Set<SelectionKey> readyChannels = selector.selectedKeys();
 
             final Iterator<SelectionKey> iter = readyChannels.iterator();
             while (iter.hasNext()) {
-                final SelectionKey key = iter.next();
-                if (key.isValid()) {
-                    if (key.isReadable()) {
-                        final SocketChannel socketChannel = (SocketChannel)key.channel();
-                        onRead(socketChannel);
-                    } else if (key.isWritable()) {
-
-                    }
-                }
+                 onSelecionKey(iter.next());
             }
         }
+    }
+
+    public void onSelecionKey(final SelectionKey key) {
+        if (!key.isValid()) {
+            return;
+        }
+
+        if (key.isReadable()) {
+            final SocketChannel socketChannel = (SocketChannel) key.channel();
+            onRead(socketChannel);
+        }
+
     }
 
     private boolean isFullMessageInBuffer() {
@@ -86,7 +92,7 @@ public class Router {
 
         if(!messageWindow.contains(message)) {
             messageWindow.add(message);
-            // retransmit the message
+            sendMessage(messageBytes);
         }
     }
 
